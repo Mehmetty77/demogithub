480e443c95f8cef89c4d265a70eb26e91e0d6ac1
Mehmetty77 <avukatmehmettoy@gmail.com>
2021-08-01 22:42:30 +0200

    firstcommit

---
+++ src/package1/ClassOne.java
@@ -0,0 +1,17 @@
+package package1;
+
+
+public class ClassOne {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        System.out.println("batch");
+        System.out.println("we like Git ");
+
+
+    }
+
+
+}
