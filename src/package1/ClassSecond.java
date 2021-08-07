d718a9ced08dcd1a528ef9de47c06e7677c40026
Mehmetty77 <avukatmehmettoy@gmail.com>
2021-08-07 21:34:56 +0200

    trialerror

--- src/package1/ClassSecond.java
+++ src/package1/ClassSecond.java
@@ -5,7 +5,7 @@ public class ClassSecond {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        
+        String str= "Java";
         
         System.out.println("This is remote repo code");
     }
