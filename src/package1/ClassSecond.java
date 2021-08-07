b765f4c343cacd32358deeaaf11959cae6640f61
Mehmetty77 <86169574+Mehmetty77@users.noreply.github.com>
2021-08-07 21:32:50 +0200

    Update ClassSecond.java

--- src/package1/ClassSecond.java
+++ src/package1/ClassSecond.java
@@ -5,7 +5,7 @@ public class ClassSecond {
     public static void main(String[] args) {
 
         System.out.println("Hello");
-        
+        int y=10;
         
         System.out.println("This is remote repo code");
     }
